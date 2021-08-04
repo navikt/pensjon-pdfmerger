b7d8307c44d4f48161c6b558f25a7940f87c9d44
monikaspko <monika.koller@nav.no>
2021-08-04 10:55:08 +0200

    PL-4140: utvidet requestobjektet

--- src/main/kotlin/no/nav/pensjon/pdfmerger/Model.kt
+++ src/main/kotlin/no/nav/pensjon/pdfmerger/Model.kt
@@ -1,5 +1,10 @@
 package no.nav.pensjon.pdfmerger
 
-data class Documentinfo(val documentName: String, val vedleggNames: List<String>?)
+data class Documentinfo(val filename: String, val dokumenttype: String, val fagomrade: String,
+                        val saknr: String, val avsenderMottaker: String, val documentName: String,
+                        val mottattSendtDato: String, val vedleggList: List<VedleggDokument>?)
 
-data class MergeInfo(val gjelderID: String, val gjelderName: String, val documentinfo: List<Documentinfo>)
+data class VedleggDokument(val filename: String, val documentName: String)
+
+data class MergeInfo(val gjelderID: String, val gjelderName: String,
+                     val documentinfo: List<Documentinfo>)
